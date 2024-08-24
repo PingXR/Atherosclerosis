6e798f623ab64be58c431405e124b3e4fd826aaa
平霄瑞 <168250723+PingXR@users.noreply.github.com>
2024-08-24 20:41:44 +0800

    Update 6-Mac_cellchat.R

--- 6-Mac_cellchat.R
+++ 6-Mac_cellchat.R
@@ -244,20 +244,14 @@ dev.off()
 num.link <- sapply(object.list, function(x) {rowSums(x@net$count) + colSums(x@net$count)-diag(x@net$count)})
 weight.MinMax <- c(min(num.link), max(num.link)) # control the dot size in the different datasets
 gg <- list()
-pdf("5-2D.pdf",height = 5,width = 10)
-par(mfrow = c(1,2), xpd=TRUE)
-for (i in 1:length(object.list)) {
-  gg[[i]] <- netAnalysis_signalingRole_scatter(object.list[[i]], title = names(object.list)[i], weight.MinMax = weight.MinMax)
-}
-patchwork::wrap_plots(plots = gg)
-dev.off()
 library(ComplexHeatmap)
 i <- 1
 pathway.union <- union(object.list[[i]]@netP$pathways,object.list[[i+1]]@netP$pathways)
+pathway.union <- c("PECAM1","CD45","TNF","EGF","VCAM","SELPLG")
 ht1 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "all", signaling = pathway.union, title = names(object.list)[i],width = 5, height = 7.5,font.size = 4,font.size.title = 6, color.heatmap = "YlGn")
 ht2 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "all", signaling = pathway.union, title = names(object.list)[i+1], width = 5, height = 7.5,font.size = 4,font.size.title = 6, color.heatmap = "YlGn")
-pdf("6-heatmap3.pdf",height = 10,width = 7)
-draw(ht1 + ht2, ht_gap = unit(0.5, "cm"))
+pdf("./supply/heatmap.pdf",height = 4,width = 7)
+draw(ht1 + ht2, ht_gap = unit(1, "cm"))
 dev.off()
 table(object.list$High@meta)
 netVisual_bubble(cellchat,
@@ -267,25 +261,6 @@ netVisual_bubble(cellchat,
                  comparison = c(1:2), angle.x = 45,remove.isolate = T,
                  signaling = c("TNF","PECAM1","VCAM","SELPLG","CD45","EGF"))   #comparison = c(1, 2)就是group里面有几组 2组 Before Late,
 ggsave("8-气泡图4.pdf",height = 4,width = 6.5)
-pathways.show <- c("EGF")
-weight.max <- getMaxWeight(object.list,slot.name = c('netP') ,attribute =pathways.show)
-pdf("EGF.pdf",height = 15,width = 10)
-par(mfrow = c(1,2), xpd=TRUE) 
-for (i in 1: length(object.list)) {
-  netVisual_aggregate(object.list[[i]],signaling = pathways.show,layout = "chord",
-                      edge.weight.max = weight.max[1],edge.width.max = 10,
-                      signaling.name = paste(pathways.show,names(object.list)[i]))
-}
-dev.off()
-pathways.show <- c("SELPLG")
-weight.max <- getMaxWeight(object.list[1],slot.name = c('netP') ,attribute =pathways.show)
-pdf("SELPLG.pdf",height = 5,width = 5)
-netVisual_aggregate(object.list[[1]],signaling = pathways.show,layout = "chord",
-                    edge.weight.max = weight.max[1],edge.width.max = 10,
-                    signaling.name = paste(pathways.show,names(object.list)[1]),
-                    pt.title = 4,title.space = 4,
-)
-dev.off()
 save(object.list, file = "~/20240103_Atherosis/v2/result/Fig_Mac/5-supply/cellchat2/METTL3/cellchat_object.list.RData")
 save(cellchat, file = "~/20240103_Atherosis/v2/result/Fig_Mac/5-supply/cellchat2/METTL3/cellchat_merged_.RData")
 # mettl3 genecor----
@@ -450,72 +425,6 @@ for(i in terms){
                        label.y = max(data2$value), size = 3)
   ggsave(paste0(a,".pdf"),height = 3,width = 1.5)
 }
-
-# --------#m6A-相关性------
-selected_genes <- c("METTL3") 
-# Ready to analyze cell type?
-selected_cell_type <- "Macrophage"
-#### 气泡图展示关注基因与通路的相关性 
-adata <- read_csv("/home/pingxr/Atherosis_0723/20230204_Atherosis/result/Macro/Macrophage/k_15/correlation/GO:BP.cor.csv")
-filtered_adata <- adata %>%
-  filter(p_value <= 0.05)
-selected_features <- c(  
-  "GOBP_RESPONSE_TO_LECTIN",
-  "GOBP_LEUKOCYTE_CELL_CELL_ADHESION",
-  "GOBP_REGULATION_OF_ANTIGEN_RECEPTOR_MEDIATED_SIGNALING_PATHWAY",
-  "GOBP_CELL_CYCLE",
-  "GOBP_CELL_CELL_ADHESION",
-  "GOBP_CYTOKINE_PRODUCTION",
-  "GOBP_REGULATION_OF_CELL_ADHESION",
-  "GOBP_CYTOKINE_MEDIATED_SIGNALING_PATHWAY",
-  "GOBP_CELL_CELL_SIGNALING",
-  "GOBP_ERBB_SIGNALING_PATHWAY",
-  "GOBP_ERBB2_SIGNALING_PATHWAY",
-  "GOBP_REGULATION_OF_ERBB_SIGNALING_PATHWAY",
-  "GOBP_INFLAMMATORY_RESPONSE"
-)
-df <- filtered_adata %>%
-  filter(feature_x %in% selected_genes,
-         feature_y %in% selected_features) 
-df$feature_y <- gsub("GOBP_", "", df$feature_y)
-df$feature_y <- gsub("_", " ", df$feature_y)
-df$feature_y <- tolower(df$feature_y)
-p <- df %>%
-  dplyr:: mutate(
-    feature_x = fct_relevel(feature_x, selected_genes),
-    feature_y = fct_relevel(feature_y, selected_features)
-  ) %>%
-  ggplot(
-    aes(x = feature_x,
-        y = feature_y)
-  ) +
-  geom_point(aes(size=-log10(p_value), color=estimate),
-             shape = 19 
-             # stroke = 3
-  ) +
-  scale_color_gradient2(low = "#0045d1", high = "red", mid = "yellow") +
-  coord_fixed() +
-  theme_bw()+
-  theme(
-    panel.grid = element_line(size = 0.2, color = "lightgrey"),
-    axis.text.x = element_text(face = "italic"),
-    axis.line = element_line(color = "black",size = 0.2)
-  ) +
-  theme(panel.grid = element_blank(), panel.background = element_blank(),
-        axis.ticks = element_blank(), legend.key = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1),
-        plot.title = element_text(hjust = 0.5)) 
-ggsave(
-  "cor.pdf",
-  plot = p,
-  height = 6,
-  width = 8,
-)
-
-
-
-
-
 
 # ythdf2 cellchat----
 library(CellChat)
@@ -753,30 +662,13 @@ pdf("3-热图.pdf",width = 8,height = 4)
 p <- gg1 + gg2
 print(p)
 dev.off()
-gg1 <- rankNet(cellchat,
-               mode = "comparison", stacked = T,
-               do.stat = TRUE) +coord_flip() +scale_fill_manual(values = c("#ab7cab", "#eaa86c"))
-gg2 <- rankNet(cellchat,
-               mode = "comparison", stacked = F, 
-               do.stat = TRUE) +coord_flip()+scale_fill_manual(values = c("#ab7cab", "#eaa86c"))
-gg1+gg2
-ggsave("5-堆叠柱形图.pdf", plot = gg1+gg2, width = 12,height = 10)
-num.link <- sapply(object.list, function(x) {rowSums(x@net$count) + colSums(x@net$count)-diag(x@net$count)})
-weight.MinMax <- c(min(num.link), max(num.link)) 
-gg <- list()
-pdf("6-2D.pdf",height = 5,width = 10)
-par(mfrow = c(1,2), xpd=TRUE)
-for (i in 1:length(object.list)) {
-  gg[[i]] <- netAnalysis_signalingRole_scatter(object.list[[i]], title = names(object.list)[i], weight.MinMax = weight.MinMax)
-}
-patchwork::wrap_plots(plots = gg)
-dev.off()
 library(ComplexHeatmap)
 i <- 1
 pathway.union <- union(object.list[[i]]@netP$pathways,object.list[[i+1]]@netP$pathways)
+pathway.union <- c("CXCL","ITGB2","GRN","TNF","NECTIN","ALCAM","CD6","VCAM")
 ht1 = netAnalysis_signalingRole_heatmap(object.list[[i]], pattern = "all", signaling = pathway.union, title = names(object.list)[i],width = 5, height = 7.5,font.size = 4,font.size.title = 6, color.heatmap = "YlGnBu")
 ht2 = netAnalysis_signalingRole_heatmap(object.list[[i+1]], pattern = "all", signaling = pathway.union, title = names(object.list)[i+1], width = 5, height = 7.5,font.size = 4,font.size.title = 6, color.heatmap = "YlGnBu")
-pdf("7-heatmap3-2.pdf",height = 10,width = 7)
+pdf("./supply/heatmap.pdf",height = 5,width = 7)
 draw(ht1 + ht2, ht_gap = unit(0.5, "cm"))
 dev.off()
 table(object.list$High@meta)
@@ -788,96 +680,7 @@ netVisual_bubble(cellchat,
                  signaling = c("CXCL","TNF","VCAM","NECTIN","ALCAM","ITGB2","CD6","GRN"),
                  comparison = c(1:2), angle.x = 45)   
 ggsave("6-气泡图1.pdf",height = 4,width = 6)
-pathways.show <- c("ITGB2")
-weight.max <- getMaxWeight(object.list,slot.name = c('netP') ,attribute =pathways.show)
-pdf("ITGB2.pdf",height = 15,width = 10)
-par(mfrow = c(1,2), xpd=TRUE)  
-for (i in 1: length(object.list)) {
-  netVisual_aggregate(object.list[[i]],signaling = pathways.show,layout = "chord",
-                      edge.weight.max = weight.max[1],edge.width.max = 10,
-                      signaling.name = paste(pathways.show,names(object.list)[i]))
-}
-dev.off()
-pathways.show <- c("CD6")
-weight.max <- getMaxWeight(object.list[1],slot.name = c('netP') ,attribute =pathways.show)
-pdf("CD6.pdf",height = 5,width = 5)
-netVisual_aggregate(object.list[[1]],signaling = pathways.show,layout = "chord",
-                    edge.weight.max = weight.max[1],edge.width.max = 10,
-                    signaling.name = paste(pathways.show,names(object.list)[1]),
-                    pt.title = 4,title.space = 4,
-)
-dev.off()
 save(object.list, file = "~/20240103_Atherosis/v2/result/Fig_Mac/5-supply/cellchat2/YTHDF2/cellchat_object.list.RData")
-# ytdhf2 genecor----
-seurat_obj <-
-  readRDS("~/20240103_Atherosis/v2/result/1-dealdata/seurat_integration_anno2sub.rds")
-seurat_obj <- NormalizeData(seurat_obj)
-seurat_obj <- subset(seurat_obj,cell_type == "Macrophage")
-selected_genes <- c("YTHDF2")
-selected_cell_type <- "Macrophage"
-outdir <- "~/20240103_Atherosis/v2/result/Fig_Mac/5-supply/3-ythdf2-geneexpr/"
-expr <- seurat_obj %>%
-  GetAssayData(assay = "RNA", slot = "data")
-median(expr[selected_genes, ])
-seurat_obj[[str_c(selected_genes, "_group")]] <-
-  if_else(expr[selected_genes,] > median(expr[selected_genes, ]),
-          "high", "low")
-print(table(seurat_obj[[str_c(selected_genes, "_group")]]))
-library(ggridges)
-cols=c("#EE757A","#A78CC1", "#DBCA85","#FFAE5D","#78ADD5","#CE9D7D","#84C780","#DED2E6","#C8E2EF")
-ggplot(df, aes(x = value, y = gene,fill=group,color=group)) + 
-  geom_density_ridges(
-    alpha = 0.6,          
-    color = 'white',      
-    rel_min_height = 0.01, 
-    scale = 1,         
-    quantile_lines = TRUE, 
-    quantiles = 2,        
-    size = 0.5,             
-    show.legend = TRUE,  
-    bandwidth = 0.2
-  ) +
-  scale_fill_manual(values = cols) + 
-  scale_color_manual(values = cols)+
-  theme_bw() +
-  ylab("Gene")+
-  xlab("Expression")+
-  scale_x_continuous(limits = c(0.5, 5), breaks = seq(0.5, 5, by = 1))
-ggsave("all2.pdf",height = 3,width = 5)
-source("/home/tutorial/20220802_scrna-m6A/code/visualization/custom_plot_function.R")
-adata <- read_csv("/home/pingxr/Atherosis_0723/20230204_Atherosis/result/Macro/Macrophage/k_15/correlation/gene.cor.csv")
-filtered_adata <- adata %>%
-  filter(p_value <= 0.05)
-selected_genes <- "YTHDF2"
-selected_cell_type <- "Macrophage"
-genes <- c("TNFRSF1B","ITGA4","ITGB1","CD6","CXCL2" ,"CXCL3" , "CXCL8" , "GRN","SORT1","ITGB2" , "ICAM1",
-           "ICAM2","NECTIN2" , "TIGIT","TNF" , "TNFRSF1A","TNF" , "VCAM1" )
-selected_features <- unique(genes)
-p <- filtered_adata %>%
-  filter(feature_x %in% selected_genes,
-         feature_y %in% selected_features) %>%
-  mutate(
-    feature_x = fct_relevel(feature_x, selected_genes),
-    feature_y = fct_relevel(feature_y, selected_features)
-  ) %>%
-  catdotplot(
-    x = feature_x,
-    y = feature_y,
-    size = -log10(p_value),
-    color = estimate,
-    title = selected_cell_type
-  ) +
-  coord_fixed() +
-  theme(
-    panel.grid = element_line(size = 0.2, color = "lightgrey"),
-    axis.text = element_text(face = "italic")
-  )
-p   
-ggsave("genes_cor_dotplot.pdf",
-       plot = p,
-       height = 3,
-       width = 3,
-)
 # ythdf2 termcor----
 adata <-readRDS( "/home/pingxr/Atherosis_0723/20230204_Atherosis/result/Macro/Macrophage/k_15/correlation/GO:BP.scores.rds" )
 seurat_obj <-readRDS( "/home/pingxr/Atherosis_0723/20230204_Atherosis/result/Macro/Macrophage/k_15/data/seurat_obj.rds" )
@@ -1000,4 +803,3 @@ ggsave(
   height = 6,
   width = 8,
 )
-
